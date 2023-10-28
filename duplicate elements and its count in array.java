41861eb12a51dcd536b2232535cfdb2957eaaafe
SAKTHI S <110224344+Sakthis17@users.noreply.github.com>
2023-10-28 15:13:30 +0530

    Add files via upload
    IN JAVA

---
+++ duplicate elements and its count in array.java
@@ -0,0 +1,44 @@
+DUPLICATE ELEMENTS AND ITS COUNT IN ARRAY: IN JAVA
+
+
+public class duplicateelementandcount { 
+	public static void main(String args[])
+	{ 
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENTER THE NO OF ELEMENTS TO ADD IN ARRAY");
+		n=sc.nextInt();
+		System.out.println("ENTER THE ELMENTS");
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		} 
+		int b=0;
+		for(int i=0;i<a.length;i++)
+		{ b=1;
+			if(a[i]!=-1)
+			{
+				for(int j=i+1;j<a.length;j++)
+				{
+					if(a[i]==a[j])
+					{
+						b++;
+						a[j]=-1;
+					}
+				} 
+				System.out.println(a[i]+"-"+b);
+			} 
+			
+		}
+		
+	}
+
+}  
+
+INPUT:[1,2,3,4,5]
+
+OUTPUT:
+1-2
+2-2
+3-1
